4b34136ea77de5add8c705e6fd04242e02b1076e
wilypomegranate <wilypomegranate@users.noreply.github.com>
2017-08-21 20:08:17 -0400

    Removed check because it doesn't work with zgen.

--- init.zsh
+++ init.zsh
@@ -10,10 +10,4 @@ load_zproj() {
     fpath=("${ZPROJ_INSTALL_DIR}/completions" $fpath)
 }
 
-# Check for ZPROJ_ROOT_DIR before bothering to load anything
-# Also make sure there is at least one project in the directory
-# This has to be exported before the module is run
-if [[ -d ${ZPROJ_ROOT_DIR} && $(ls ${ZPROJ_ROOT_DIR} | wc -l) > 0 ]]
-then
-    load_zproj
-fi
+load_zproj
